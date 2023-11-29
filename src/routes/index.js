56465b1ceb25e6c17d0e74efaebee835479423cb
DawoodAli23 <tatwatatwa007@gmail.com>
2023-11-29 21:49:33 +0500

    auth middlewares

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authRoutes = require("./auth.routes");
 const paymentRoutes = require("./payment.routes");
-router.use("/auth", authRoutes).use("/payment", paymentRoutes);
+const { userMiddleware } = require("../middleware/jwt");
+router.use("/auth", authRoutes).use("/payment", userMiddleware, paymentRoutes);
 module.exports = router;
