dd4ec1b2c45c41f9b748699563a84745ac493f5b
DawoodAli23 <tatwatatwa007@gmail.com>
2023-12-03 03:51:40 +0500

    fixes

--- src/routes/coupon.routes.js
+++ src/routes/coupon.routes.js
@@ -1,5 +1,17 @@
 const express = require("express");
-const { createCoupon } = require("../controller/coupon.controller");
+const {
+  createCoupon,
+  getCoupons,
+  editCoupon,
+  deleteCoupon,
+  availCoupon,
+} = require("../controller/coupon.controller");
+const { adminMiddleware } = require("../middleware/jwt");
 const router = express.Router();
-router.post("/", createCoupon);
+router
+  .post("/", adminMiddleware, createCoupon)
+  .get("/", adminMiddleware, getCoupons)
+  .put("/", adminMiddleware, editCoupon)
+  .delete("/:_id", adminMiddleware, deleteCoupon)
+  .post("/:code", availCoupon);
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const paymentRoutes = require("./payment.routes");
 const categoryRoutes = require("./category.routes");
 const paymentPackagesRoutes = require("./paymentPackages.routes");
 const liveTvRoutes = require("./liveTv.routes");
+const couponRoutes = require("./coupon.routes");
 const { userMiddleware, adminMiddleware } = require("../middleware/jwt");
 router
   .use("/auth", authRoutes)
   .use("/payment", userMiddleware, paymentRoutes)
   .use("/packages", userMiddleware, paymentPackagesRoutes)
   .use("/category", categoryRoutes)
-  .use("/liveTV", liveTvRoutes);
+  .use("/liveTV", liveTvRoutes)
+  .use("/coupon", userMiddleware, couponRoutes);
 module.exports = router;
