5d4ee6699d97b0b85084a90eff6df7ef7f0e0841
DawoodAli23 <tatwatatwa007@gmail.com>
2023-12-02 18:37:05 +0500

    cors

--- index.js
+++ index.js
@@ -3,16 +3,11 @@ const cron = require("node-cron");
 const app = express();
 require("dotenv").config();
 require("./src/config/connection")();
-const routes = require("./src/routes");
-const { verifyPayments } = require("./src/jobs/verifyPayment");
+var cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 app.use(require("./src/routes"));
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at PORT ${process.env.PORT}`);
 });
-
-//jobs
-
-// cron.schedule("1 * * * * *", () => {
-//   verifyPayments();
-// });
