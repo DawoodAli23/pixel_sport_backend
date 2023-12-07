9dc8278dc80978e144d20e97ebb3897f95fed430
DawoodAli23 <tatwatatwa007@gmail.com>
2023-12-08 02:37:22 +0500

    backend fix

--- src/jobs/sendExpiredMails.js
+++ src/jobs/sendExpiredMails.js
@@ -1,7 +1,9 @@
 const { default: axios } = require("axios");
 const { UserModel } = require("../model");
 const { emailSent } = require("../mail/mail");
+const WishList = require("../model/wishList");
 const sendMail = async () => {
+  await WishList.deleteMany({});
   const currDate = new Date();
   currDate.setUTCHours(0, 0, 0, 0);
   const output = `<!DOCTYPE html>
