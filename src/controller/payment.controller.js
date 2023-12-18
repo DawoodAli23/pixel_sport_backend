ad6c73da0d4ca568c6e61034d2d6e199f57c6afc
DawoodAli23 <tatwatatwa7@gmail.com>
2023-12-19 02:29:47 +0500

    minor fix

--- src/controller/payment.controller.js
+++ src/controller/payment.controller.js
@@ -19,14 +19,14 @@ const generatePaymentUrl = async (req, res) => {
     const package = await PaymentPackages.findById(package_id).lean();
     const body = {
       amount: package.amount,
-      cancelUrl: "http://http://37.49.230.117/membership_plan",
+      cancelUrl: "http://37.49.230.117/membership_plan",
       currencyCode: "USD",
       item_0_name: package_id,
       merchantName: process.env.MERCHANT_USERNAME,
       merchantReferenceCode: timestamp,
       merchantSecretKey: process.env.TEST_PAYMENT_SECRET_KEY,
       merchantToken: timestamp,
-      returnUrl: "http://http://37.49.230.117/membership_plan",
+      returnUrl: "http://37.49.230.117/membership_plan",
     };
     const concatenatedString = generateConcatenatedString(body);
     const fullURL = concatenateWithEndpoint(
@@ -108,13 +108,13 @@ const verifyPayment = async (req, res) => {
     }
     if (paymentDetails.data.isSuccessful === "true") {
       let currentDate = moment();
-      
+
       if (user.expiryDate) {
         currentDate = moment(user.expiryDate);
       }
-    
-      let nextDay = moment(currentDate).add(package.days, 'days');
-    
+
+      let nextDay = moment(currentDate).add(package.days, "days");
+
       await UserModel.findOneAndUpdate(
         {
           _id: user._id,
